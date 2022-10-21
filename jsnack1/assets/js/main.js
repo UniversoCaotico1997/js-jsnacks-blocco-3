e1b8190366f01fde5cfe97e10037583c449539a0
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-21 11:53:55 +0200

    benzina diesel

--- jsnack1/assets/js/main.js
+++ jsnack1/assets/js/main.js
@@ -4,3 +4,75 @@
 // Infine stampa separatamente i 3 array.
 
 
+const listaAuto = [
+    {
+        marca:`Audi`,
+        modello:`Q3`,
+        alimentazione:`diesel`
+    },
+    {
+        marca:`Audi`,
+        modello:`A3`,
+        alimentazione:`diesel`
+    },
+    {
+        marca:`Audi`,
+        modello:`Q5`,
+        alimentazione:`diesel`
+    },
+    {
+        marca:`Audi`,
+        modello:`Rs3`,
+        alimentazione:`benzina`
+    },
+    {
+        marca:`Audi`,
+        modello:`Rs6`,
+        alimentazione:`benzina`
+    },
+    {
+        marca:`Fiat`,
+        modello:`Panda`,
+        alimentazione:`gpl`
+    },
+    {
+        marca:`Mercedez`,
+        modello:`ClA`,
+        alimentazione:`diesel`
+    },
+    {
+        marca:`Bmw`,
+        modello:`M4`,
+        alimentazione:`benzina`
+    },
+    {
+        marca:`Tesla`,
+        modello:`model x`,
+        alimentazione:`elettrico`
+    },
+    {
+        marca:`Tesla`,
+        modello:`model s`,
+        alimentazione:`elettrico`
+    },
+]
+
+
+
+
+const benzina = listaAuto.filter(macchina => {
+    if (macchina.alimentazione === `benzina`) {
+        return true
+        
+    }
+});
+ console.log(benzina);
+
+ const diesel = listaAuto.filter(macchina => {
+    if (macchina.alimentazione === `diesel`) {
+        return true
+        
+    }
+});
+
+console.log(diesel);
